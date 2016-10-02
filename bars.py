ea40fd9b3a50762234faed8ed69e583b8006508a
Odselmaa <d.odselmaa@yahoo.com>
2016-10-02 16:05:36 +0000

    wrote readme.md, corrected some stuff

--- bars.py
+++ bars.py
@@ -6,37 +6,55 @@ def load_data(filepath):
         with open(filepath, 'r') as file_handler:
             return json.load(file_handler)
     
+    
+def get_biggest_bar(bar_list):
+    bar_list_tmp = sort_by_cells_item(bar_list)
+    return bar_list_tmp[-1]
 
+def get_smallest_bar(bar_list):
+    bar_list_tmp = sort_by_cells_item(bar_list)
+    return bar_list_tmp[0]
+    
+
+
+
+def get_closest_bar(bar_list, longitude, latitude):
+    
+    
+    return min(bar_list, key=lambda bar: get_distance(bar['Cells']['geoData']['coordinates'][1],latitude,\
+    bar['Cells']['geoData']['coordinates'][0], longitude))
+    
 def get_bar_seat(bar):
     return bar['Cells']['SeatsCount']
     
 def sort_by_cells_item(bar_list):
     return  sorted(bar_list, key = get_bar_seat, reverse=False)
+    
+def get_name(bar):
+    return bar["Cells"]["Name"]
+    
+def get_address(bar):
+    return bar["Cells"]["Address"]
+    
 
 def get_distance(long1, lat1, long2, lat2):
     return ((long1 - long2)**2 + (lat1 - lat2)**2)**0.5
     
-def get_biggest_bar(bar_list):
-    bar_list_tmp = sort_by_cells_item(bar_list)
-    print(bar_list_tmp[-1])
-
-def get_smallest_bar(bar_list):
-    bar_list_tmp = sort_by_cells_item(bar_list)
-    print(bar_list_tmp[0])
-
-def get_closest_bar(bar_list, longitude, latitude):
-    
-    
-    return min(bar_list, key=lambda bar: ((bar['Cells']['geoData']['coordinates'][1] - latitude)**2 +\
-    (bar['Cells']['geoData']['coordinates'][0] - longitude)**2)**0.5)
-
 if __name__ == '__main__':
+
     file_path = 'bars.json'
     data = load_data(file_path)
-    print(get_biggest_bar(data))
-    print(get_smallest_bar(data))
+    
+    smallest_bar = get_smallest_bar(data)
+    biggest_bar = get_biggest_bar(data)
+    
+    print("\nBiggest bar in Moscow is: '%s', %s" % (get_name(biggest_bar), get_address(biggest_bar)))
+    print("Smallest bar in Moscow is: '%s', %s" % (get_name(smallest_bar), get_address(smallest_bar)))
 
+    print("\nDo you want to find where is closest bar?")
     longitude = float(input("Enter longitude: "))
     latitude = float(input("Enter latitude: "))
+    
+    closest_bar = get_closest_bar(data, longitude, latitude)
 
-    print(get_closest_bar(data, longitude, latitude))
+    print("Closest bar is : '%s', %s" % (get_name(closest_bar), get_address(closest_bar)))
